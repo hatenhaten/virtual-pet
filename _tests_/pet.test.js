29353805172e6b4812bec52a4b7cc16db66c0af6
Kallum Tolkien <kallumtolkien@gmail.com>
2021-01-15 12:44:52 +0000

    Added method which ages the pet

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -7,7 +7,23 @@ describe('constructor', () => {
 
     it('sets the name property', () => {
         const pet = new Pet('Fido'); 
+
         expect(pet.name).toEqual('Fido');
     });
 
-  });
+    it('has an initial age of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.age).toEqual(0);
+    });
+});
+
+describe('growUp', () => {
+    it('increments age by 1', () => {
+    const pet = new Pet('Fido');
+    pet.growUp();
+
+    expect(pet.age).toEqual(1);
+    });
+});
+  
