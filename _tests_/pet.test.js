c6f73770b3953f5b0d09080647b429d48d8b39fe
Kallum Tolkien <kallumtolkien@gmail.com>
2021-01-17 13:52:25 +0000

    walk method added to our pet

--- _tests_/pet.test.js
+++ _tests_/pet.test.js
@@ -16,14 +16,64 @@ describe('constructor', () => {
 
         expect(pet.age).toEqual(0);
     });
+
+    it('has an initial hunger of 0', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.hunger).toEqual(0);
+    });
+
+    it('has an initial fitness of 10', () => {
+        const pet = new Pet('Fido');
+
+        expect(pet.fitness).toEqual(10);
+    });
 });
 
 describe('growUp', () => {
     it('increments age by 1', () => {
-    const pet = new Pet('Fido');
-    pet.growUp();
+        const pet = new Pet('Fido');
+        pet.growUp();
 
-    expect(pet.age).toEqual(1);
+        expect(pet.age).toEqual(1);
+    });
+
+    it('increments hunger by 5', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    });
+
+    it('decreases fitness by 3', () => {
+        const pet = new Pet('Fido');
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
     });
 });
+
+describe('walk', () => {
+    it('increases fitness by 4', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 4;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(8);
+    });
+
+
+    it('stops increasing fitness at a value of 10', () => {
+        const pet = new Pet('Fido');
+
+        pet.fitness = 8;
+        pet.walk();
+
+        expect(pet.fitness).toEqual(10);
+    });
+
+
+})
+
   
